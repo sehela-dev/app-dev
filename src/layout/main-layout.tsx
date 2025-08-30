bbc1301a1728e13ae0348d7ea2ce463079b0c8e3
sehela-dev <seheladev@gmail.com>
2025-08-30 21:53:47 +0700

    home-page

--- src/layout/main-layout.tsx
+++ src/layout/main-layout.tsx
@@ -3,12 +3,12 @@ import { MainHeaderComponent, MainBottomNav } from "@/components/layout";
 export default function MainLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="h-dvh flex flex-col sm:bg-brand-100 w-[100vw]">
-      <div className="mx-auto w-full max-w-[389px] sm:max-w-[667px] bg-brand-50 h-dvh flex flex-col">
+      <div className="sm:mx-auto w-full max-w-dvw sm:max-w-[667px] bg-brand-50 h-dvh flex flex-col">
         {/* Header (fixed height, non-scrolling) */}
         <MainHeaderComponent />
 
         {/* Content (only this scrolls) */}
-        <main className="flex-1 min-h-0 overflow-y-auto">{children}</main>
+        <main className="flex-1 min-h-0 overflow-y-auto pb-6">{children}</main>
 
         {/* Footer / mobile menu (fixed height, non-scrolling) */}
         <MainBottomNav />
